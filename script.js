f6a2392b65708aff0a77460bab14968fa84c7026
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-09 18:21:01 +0000

    added getUserChoice

---
+++ script.js
@@ -0,0 +1,8 @@
+const getUserChoice = userInput  => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+    return userInput;
+  } else {
+    console.log('Error - please submit a valid input')
+  }
+}; 
