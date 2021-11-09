c6ee300c0dbd45a1b36fd729afed6b16f6b55ef7
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-09 18:21:29 +0000

    added getComputerChoice

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const getUserChoice = userInput  => {
     console.log('Error - please submit a valid input')
   }
 }; 
+function getComputerChoice(){
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice < 1) {
+    return 'rock'
+  } else if (computerChoice < 2) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
